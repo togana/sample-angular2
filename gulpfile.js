9b67ffb9fbe8138a02d2cec6f1e85156bdd71940
togana <o.togana@gmail.com>
2016-05-01 17:03:05 +0900

    add index.html and build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var path = require('path');
 var sourcemaps = require('gulp-sourcemaps');
 var ts = require('gulp-typescript');
 var del = require('del');
@@ -24,13 +25,34 @@ gulp.task('build:server', function () {
   	.pipe(gulp.dest('dist'))
 });
 
+// CLIENT
+
+// jsNPMDependencies, sometimes order matters here! so becareful!
+var jsNPMDependencies = [
+  'angular2/bundles/angular2-polyfills.js',
+  'systemjs/dist/system.src.js',
+  'rxjs/bundles/Rx.js',
+  'angular2/bundles/angular2.dev.js',
+  'angular2/bundles/router.dev.js'
+]
+
+// build index
+gulp.task('build:index', function(){
+  var mappedPaths = jsNPMDependencies.map(file => {return path.resolve('node_modules', file)})
+  var copyJsNPMDependencies = gulp.src(mappedPaths, {base:'node_modules'})
+    .pipe(gulp.dest('dist/libs'))
+  var copyIndex = gulp.src('client/index.html')
+    .pipe(gulp.dest('dist'))
+  return [copyJsNPMDependencies, copyIndex];
+});
+
 // build
 gulp.task('build', function(callback){
   runSequence(
     'build:clean',
     'build:server',
-    callback
-  );
+    'build:index',
+    callback);
 });
 
 gulp.task('default', ['build']);
